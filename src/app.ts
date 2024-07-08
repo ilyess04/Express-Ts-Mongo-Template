1ef5c3248673dda60e94abf8dac7195298af6fac
Sellamingo <ilyessellami04@gmail.com>
2024-07-08 21:44:39 +0100

    use express json

--- src/app.ts
+++ src/app.ts
@@ -40,6 +40,8 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options);
 
+app.use(express.json());
+
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/", appRoutes);
