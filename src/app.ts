00a56dedf88f0428f623ac8a19c3a73113e60c06
Sellamingo <ilyessellami04@gmail.com>
2024-07-08 19:32:50 +0100

    Configure swagger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,39 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_: Request, res: Response) => {
-  res.send("Hello World!");
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express TypeScript API',
+    version: '1.0.0',
+    description: 'API documentation for the Express TypeScript application',
+  },
+  servers: [
+    {
+      url: `http://localhost:${PORT}`,
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./src/**/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/', (_: Request, res: Response) => {
+  res.send('Welcome To ExpressJS Typescript && MongoDB Template!');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api/docs`);
 });
