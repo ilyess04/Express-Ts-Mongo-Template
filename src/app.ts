17e3c5f0cfda20c778d14618f230d206255700c7
Sellamingo <ilyessellami04@gmail.com>
2024-07-08 19:19:27 +0100

    Init express && typescript

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_: Request, res: Response) => {
   res.send("Hello World!");
 });
 
